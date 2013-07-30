bf317d2bda25e7688485d8dbf5f05bd62c0d8ef0
Joshua <jbkunst@gmail.com>
2013-07-30 12:55:07 -0400

    truncate fix

--- R/others.R
+++ R/others.R
@@ -1,4 +1,4 @@
-truncate <- <- function(x, inf = -Inf, sup = Inf){
+truncate <- function(x, inf = -Inf, sup = Inf){
   if(missing(inf)) return(ifelse(x<sup,x,sup))
   if(missing(sup)) return(ifelse(x>inf,x,inf))
   x <- ifelse(x<sup,x,sup)
