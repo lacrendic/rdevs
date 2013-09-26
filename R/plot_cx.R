14fa190fd6dcd4d9fce284451a486aae7a4ebc9c
Joshua <jbkunst@gmail.com>
2013-09-26 15:57:42 -0300

    creating new function

--- R/plot_cx.R
+++ R/plot_cx.R
@@ -1,12 +1,19 @@
-plot_dist_by_var <- function(dataframe, split.var, file = "output_plot_dist_by_var.pdf", ...){
+plot_df_dist_formula <- function(dataframe, facet_formula, file = "output_plot_dist_by_var.pdf", ...){
   library(ggplot2)
   pdf(file, ...)
-  l_ply(setdiff(names(dataframe), split.var), function(namevar){
+  facet_vars <- setdiff(as.character(facet_formula), c("~", "."))
+  
+  l_ply(setdiff(names(dataframe), facet_vars), function(namevar){
     message(sprintf("plotting %s", namevar))
-    daux <- subset(dataframe, select = c(namevar, split.var))
-    names(daux) <- c("namevar", "split.var")
-    p <- ggplot(daux) + geom_bar(aes(x=namevar)) + facet_grid(split.var ~ .)  + ggtitle(namevar)
+    daux <- subset(dataframe, select = c(namevar, facet_vars))
+    names(daux) <- c("namevar", facet_vars)
+    p <- ggplot(daux) +
+      
+      geom_bar(aes(x=namevar)) +
+      facet_grid(facet_formula, scales="free") +
+      ggtitle(str_capitalize(namevar))
     print(p)
   })
   dev.off()
 }
+
