a8a6e11cf844f4e852386f20106d6b4e7e8fddce
Joshua <jbkunst@gmail.com>
2013-10-01 16:12:29 -0300

    updating plot_df_dist function

--- R/plot_cx.R
+++ R/plot_cx.R
@@ -1,61 +1,49 @@
-plot_df_dist <- function(dataframe, facet_formula, file = "output_plot_dist_by_var.pdf", ...){
-  library(ggplot2)
-  pdf(file, ...)
-  
-  if(!missing(facet_formula)){
-    facet_vars <- setdiff(as.character(facet_formula), c("~", "."))  
-  }
-  
-  for(namevar in setdiff(names(dataframe), facet_vars)){
-    message(sprintf("plotting %s", namevar))
-    daux <- subset(dataframe, select = c(namevar, facet_vars))
-    names(daux) <- c("namevar", facet_vars)
-    p <- ggplot(daux) + geom_bar(aes(x=namevar)) + xlab(NULL) + ylab(NULL) +  ggtitle(str_capitalize(namevar))   
-    if(!missing(facet_formula)){
-      p <- p  + facet_grid(facet_formula, scales="free")
-    }
-    print(p)
-  }
-  dev.off()
-}
-
-
-
-plot_df_dist_2 <- function(dataframe,
-                           facet_formula,
-                           names.responses,
-                           file = "output_plot_dist_by_var.pdf",
-                           ...){
+plot_df_dist <- function(df, facet_formula, responses, file = "output_plot_dist_by_var.pdf", ...){
   library(ggplot2)
   library(scales)
   
   pdf(file, ...)
   colors <- c("#A61000", "#1B0773", "#007D1C", "#FFCE00")
   
+  namevars <- names(df)
   if(!missing(facet_formula)){
-    facet_vars <- setdiff(as.character(facet_formula), c("~", "."))  
+    facet_vars <- setdiff(as.character(facet_formula), c("~", "."))
+    namevars <- setdiff(namevars, facet_vars)
   }
-
-  for(namevar in setdiff(names(dataframe), facet_vars)){
-   
+  if(!missing(responses)){
+    responses_var <- names(responses)
+    namevars <- setdiff(namevars, responses_var)
+  }
+  
+  for(namevar in namevars){
     message(sprintf("plotting %s", namevar))
-    daux <- subset(dataframe, select = c(namevar, facet_vars, responses))
-    names(daux)[1] <- "namevar"
-    head(daux)
-    p <- ggplot(daux) +  
-      geom_bar(aes(x=namevar, y=(..count..)/sum(..count..))) +
-      ylab(NULL) + xlab(NULL) +
-      ggtitle(str_capitalize(namevar))
-    if(!missing(names.responses)){
-      for(response in names.responses){
-        color <- colors[which(response == names.responses)]
-        if(!is.numeric(daux[["namevar"]])){
+    daux <- subset(df, select=namevar)
+    
+    if(!missing(facet_formula)){
+      daux <- cbind(daux, subset(df, select=facet_vars))
+    }
+    if(!missing(responses)){
+      daux <- cbind(daux, subset(df, select=names(responses)))
+    }
+    
+    # Base plot
+    p <- ggplot(daux, aes_string(x=namevar)) +
+      geom_bar(aes(y=(..count..)/sum(..count..))) +
+      xlab(NULL) + ylab(NULL) +  ggtitle(str_capitalize(namevar))
+    
+    if(!missing(responses)){
+      for(response in names(responses)){
+        color <- colors[which(response == names(responses))]
+        if(is.numeric(daux[[namevar]])){
+          p <- p + stat_smooth(data=daux, aes_string(x=namevar,y=response), colour = color)
+        } else{
           p <- p +
-            stat_summary(aes_string(x="namevar", y=response), fun.y=mean, colour=color, geom="point") +
-            stat_summary(aes_string(x="namevar", y=response, group = 1), fun.y=mean, colour=color, geom="line")
+            stat_summary(data=daux, aes_string(x=namevar, y=response), fun.y=mean, colour=color, geom="point") +
+            stat_summary(data=daux, aes_string(x=namevar, y=response, group = 1), fun.y=mean, colour=color, geom="line")
         }
       }
     }
+    
     if(!missing(facet_formula)){
       p <- p  + facet_grid(facet_formula, scales="free")
     }
