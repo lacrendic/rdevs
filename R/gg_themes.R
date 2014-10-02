9ec32a44b2027916b4994eb76dc80d3c613a04a2
Joshua <jbkunst@gmail.com>
2014-10-02 14:46:03 -0300

    minimal change on theme_hc

--- R/gg_themes.R
+++ R/gg_themes.R
@@ -1,6 +1,6 @@
 theme_hc <- function(){
   theme(
-    text                = element_text(size = 10),
+    text                = element_text(size = 11),
     title               = element_text(hjust=0), 
     axis.title.x        = element_text(hjust=.5),
     axis.title.y        = element_text(hjust=.5),
@@ -15,9 +15,9 @@ theme_hc <- function(){
   )
 }
 
-colors_hc <- function(){
+theme_hc_colors <- function(){
   c("#7CB5EC", "#313131", "#F7A35C", "#90EE7E", "#7798BF", "#AAEEEE",
-               "#FF0066", "#EEAAEE", "#55BF3B", "#DF5353", "#7798BF", "#AAEEEE")
+	"#FF0066", "#EEAAEE", "#55BF3B", "#DF5353", "#7798BF", "#AAEEEE")
 }
 
 
