715cabfe757170fa0d1852f71b9002fea7fb7d85
mhramire <mhramire@uc.cl>
2014-10-13 17:39:13 -0300

    smooth method changed to gam in plot_dist function

--- R/plot.R
+++ R/plot.R
@@ -182,7 +182,7 @@ plot_dist <- function(variable, indicator,  facet){
   
   if(!missing(indicator)){
     if(is.numeric(variable)){
-      p <- p + stat_smooth(aes(x=variable,y=indicator), color ="darkred", se=FALSE)
+      p <- p + stat_smooth(aes(x=variable,y=indicator), color ="darkred",method="gam", se=FALSE)
     } else{
       p <- p +
         stat_summary(aes(x=variable,y=indicator), fun.y=mean, colour="red", geom="point") +
