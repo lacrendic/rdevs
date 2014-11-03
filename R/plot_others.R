2ecc3a792ca8a450c3bb7ba0186d993271ab7310
mhramire <mhramire@uc.cl>
2014-11-03 18:27:20 -0300

    se agregó función plot_complete_df2

--- R/plot_others.R
+++ R/plot_others.R
@@ -109,3 +109,61 @@ plot_complete_df <- function(df, facet_formula, responses,smooth_span=0.99, file
   }
   dev.off()
 }
+
+plot_complete_df2 <- function (df, facet_formula, responses, smooth_span = 0.99, file = "output_plot_dist_by_var.pdf", ...) 
+{
+  require(ggplot2)
+  require(scales)
+  pdf(file, ...)
+  colors <- c("#A61000", "#1B0773", "#007D1C", "#FFCE00")
+  namevars <- names(df)
+  if (!missing(facet_formula)) {
+    facet_vars <- setdiff(as.character(facet_formula), c("~","."))
+    namevars <- setdiff(namevars, facet_vars)
+  }
+  if (!missing(responses)) {
+    responses_var <- names(responses)
+    namevars <- setdiff(namevars, responses_var)
+  }
+  for (namevar in namevars) {
+    message(sprintf("plotting %s", namevar))
+    daux <- subset(df, select = namevar)
+    if (!is.numeric(daux[[namevar]])) {
+      daux[[namevar]] <- addNA(daux[[namevar]])
+    }
+    if (!missing(facet_formula)) {
+      daux <- cbind(daux, subset(df, select = facet_vars))
+    }
+    if (!missing(responses)) {
+      daux <- cbind(daux, subset(df, select = names(responses)))
+    }
+    p <- ggplot(daux, aes_string(x = namevar)) + geom_bar(aes(y = (..count..)/sum(..count..)),fill="gray80") +
+      xlab(NULL) + ylab(NULL) + ggtitle(str_capitalize(namevar)) + scale_y_continuous(labels = percent)
+    if (!missing(responses)) {
+      for (response in names(responses)) {
+        color <- colors[which(response == names(responses))]
+        if (is.numeric(daux[[namevar]])) {
+          p <- p + stat_smooth(data = daux, aes_string(x = namevar, y = response), colour = color, method = "loess", 
+                               span = smooth_span, se = T, fill = "gray", alpha = 0.05)
+        }
+        else {
+          p <- p + stat_summary(data = daux, aes_string(x = namevar,  y = response), fun.y = mean, colour = color, 
+                                geom = "point") + stat_summary(data = daux, aes_string(x = namevar, y = response, group = 1), 
+                                                               fun.y = mean, colour = color, geom = "line")
+        }
+      }
+    }
+    if (!missing(facet_formula)) {
+      p <- p + facet_grid(facet_formula, scales = "free") + 
+        theme(text = element_text(size = 10), panel.grid = element_blank(), 
+              panel.border = element_rect(color="grey80", size=0.5, linetype="solid",fill=NA), panel.background = element_blank(),
+              legend.position = "bottom", legend.title = element_blank(),
+              strip.text.x = element_text(size=9,colour="black"),
+              strip.background = element_rect(colour="#D6D6D6", fill="#D6D6D6"),
+              axis.text.x = element_text(angle = 45, hjust = 1,colour="black",size=4),
+              axis.text.y = element_text(colour="black",size=8))
+    }
+    print(p)
+  }
+  dev.off()
+}
